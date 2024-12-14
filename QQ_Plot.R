076a13189cdeb2dc40fd1a1fce5cc21adf27729d
KayBee-A <kbatool16@gmail.com>
2024-12-14 22:56:11 +0200

    initial

---
+++ QQ_Plot.R
@@ -0,0 +1,9 @@
+
+#Make a qq plot
+png("GWAS_BMI.png")
+qq(GWAS_BMI$LOG10P,
+   main = "Q-Q plot BMI_GWAS",
+   xlim = c(0, 7), ylim = c(0, 12), pch = 18,
+   col = "blue4", cex = 1.5, las = 1)
+
+dev.off()
